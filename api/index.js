e090257b885588af6ffd309786b529919743f0e7
Vidisha151 <2206146@kiit.ac.in>
2026-04-09 02:06:39 +0530

    fixed vercel api crash

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 export default function handler(req, res) {
-  res.status(200).json({
-    message: "API is working 🚀",
-    author: "Vidisha Kashyap"
-  });
+  try {
+    res.status(200).json({
+      message: "API working perfectly 🚀",
+      name: "Vidisha"
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Something went wrong"
+    });
+  }
 }
